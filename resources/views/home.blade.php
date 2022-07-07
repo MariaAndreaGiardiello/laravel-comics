6218eeae5838c5cabcc6806f77e345e73514da62
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-07-07 16:17:24 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOME</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, quos aliquid. Exercitationem, vitae possimus dolores soluta id atque sequi eligendi assumenda facere a, nesciunt aliquam veritatis rem expedita, qui nihil!</p>
+</body>
+</html>
